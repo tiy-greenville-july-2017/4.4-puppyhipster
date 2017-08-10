a7a654588efa03d67f0c81e34d03d5421438d737
Dan Dietz <danddietz@gmail.com>
2017-08-10 11:51:20 -0400

    added kepler

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,7 +4,8 @@ let puppyTemplate = require('./templates/puppy.hbs');
 
 let puppies = [
   {name: 'Oscar', pic: 'http://68.media.tumblr.com/tumblr_lnv7t4syE31qb0fx9o1_500.jpg'},
-  {name: 'Scooby', pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbY4f4apAMR0k6JYx7_f9eVcYnimkJejkWdDDoPPgTfbOlX96j'}
+  {name: 'Scooby', pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbY4f4apAMR0k6JYx7_f9eVcYnimkJejkWdDDoPPgTfbOlX96j'},
+  {name: 'Kepler', pic: 'http://darlingdapperdachshund.com/assets/images/kepler/9.jpg'}
 ];
 
 let $container = $('.puppy-container');
